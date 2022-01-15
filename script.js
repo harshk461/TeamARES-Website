bdc6ebd38e61b035a729f2053018404971292a6e
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2022-01-15 13:23:29 +0530

    Teams CORS resolved

--- script.js
+++ script.js
@@ -1,6 +1,340 @@
 // For Teams Page
 
-$.get("https://opensheet.elk.sh/1o4WOXewxr_uh70EzAjx9O7JwM3E7EUDrZrR0HrL3Src/1", function (data) {
+const data = [{
+  "year": "alpha",
+  "priority": "1",
+  "firstname": "Arjun",
+  "lastname": "Singh",
+  "por": "President (2021-2022)",
+  "image": "./team/Arjun_President.png",
+  "linkedin": "https://www.linkedin.com/in/arjun-singh-/"
+},
+{
+  "year": "alpha",
+  "priority": "2",
+  "firstname": "Satvik",
+  "lastname": "Sharma",
+  "por": "President (2021-2022)",
+  "image": "./team/satvik_sharma.png"
+},
+{
+  "year": "alpha",
+  "priority": "3",
+  "firstname": "Swaranjeet",
+  "lastname": "Singh",
+  "por": "Vice President (2021-2022)",
+  "image": "./team/swaranjeet.png",
+  "linkedin": "https://www.linkedin.com/in/swaranjeet-s-555118178"
+},
+{
+  "year": "alpha",
+  "priority": "4",
+  "firstname": "Vineet",
+  "lastname": "Agarwal",
+  "por": "Vice President (2021-2022)",
+  "image": "./team/Vineet_VP.png",
+  "linkedin": "https://www.linkedin.com/in/vineet-agarwal-188690183/"
+},
+{
+  "year": "alpha",
+  "priority": "5",
+  "firstname": "Harsh",
+  "lastname": "Gupta",
+  "por": "Mentor (2021-2022)",
+  "image": "./team/Harsh_Mentor.png",
+  "linkedin": "https://www.linkedin.com/in/harsh-gupta-202418187/"
+},
+{
+  "year": "alpha",
+  "priority": "6",
+  "firstname": "Manan",
+  "lastname": "Madan",
+  "por": "Mentor (2021-2022)",
+  "image": "./team/Manan_Mentor.png",
+  "linkedin": "https://www.linkedin.com/in/manan-madan-646950167/"
+},
+{
+  "year": "alpha",
+  "priority": "7",
+  "firstname": "Mayank",
+  "lastname": "Pandit",
+  "por": "Mentor (2021-2022)",
+  "image": "./team/mayank_pandit.png",
+  "linkedin": "https://www.linkedin.com/in/mayank-pandit-516494a8"
+},
+{
+  "year": "alpha",
+  "priority": "8",
+  "firstname": "Parmesh",
+  "lastname": "Singh",
+  "por": "Mentor (2021-2022)",
+  "image": "./team/parmesh.jpg",
+  "linkedin": "https://www.linkedin.com/in/parmesh-singh-728689170/"
+},
+{
+  "year": "alpha",
+  "priority": "9",
+  "firstname": "Akshat",
+  "lastname": "Wadhwa",
+  "por": "Mentor (2021-2022)",
+  "image": "./team/Akshat_Wadhwa_Mentor.png",
+  "linkedin": "https://www.linkedin.com/in/akshat-wadhwa-9928b21b2/"
+},
+{
+  "year": "alpha",
+  "priority": "10",
+  "firstname": "Gaurav",
+  "lastname": "Gupta",
+  "por": "Mentor (2021-2022)",
+  "image": "./team/Gaurav_Mentor.png",
+  "linkedin": "https://www.linkedin.com/in/gaurav-gupta-ab4a35199/"
+},
+{
+  "year": "alpha",
+  "priority": "11",
+  "firstname": "Himanshu",
+  "lastname": "Singhal",
+  "por": "Mentor (2021-2022)",
+  "image": "./team/Himanshu_Singhal_Mentor.png",
+  "linkedin": "https://www.linkedin.com/in/himanshu-singhal-06b3531a1/"
+},
+{
+  "year": "alpha",
+  "priority": "12",
+  "firstname": "Pranay",
+  "lastname": "Kothari",
+  "por": "Team Lead (2021-2022)",
+  "image": "./team/Pranay_TeamLead.png",
+  "linkedin": "https://www.linkedin.com/in/pranay-kothari-"
+},
+{
+  "year": "alpha",
+  "priority": "13",
+  "firstname": "Kunal",
+  "lastname": "",
+  "por": "Team Lead (2021-2022)",
+  "image": "./team/Kunal.png",
+  "linkedin": "https://www.linkedin.com/in/kunal-chauhan-a49778191"
+},
+{
+  "year": "alpha",
+  "priority": "14",
+  "firstname": "Tushar",
+  "lastname": "Wadhwa",
+  "por": "Team Co-Lead (2021-2022)",
+  "image": "./team/Tushar_Business.png"
+},
+{
+  "year": "alpha",
+  "priority": "15",
+  "firstname": "Vicky",
+  "lastname": "Jaiswal",
+  "por": "Team Co-Lead & Electronics Lead (2021-2022)",
+  "image": "./team/Vicky.png"
+},
+{
+  "year": "alpha",
+  "priority": "16",
+  "firstname": "Daksh",
+  "lastname": "Gupta",
+  "por": "Software Lead (2021-2022)",
+  "image": "./team/Daksh_Software.png"
+},
+{
+  "year": "alpha",
+  "priority": "17",
+  "firstname": "Sourabh",
+  "lastname": "",
+  "por": "Software Lead (2021-2022)",
+  "image": "./team/Sourabh_Sw.png",
+  "linkedin": "https://www.linkedin.com/in/sourabh-choudhary-a817401b6"
+},
+{
+  "year": "alpha",
+  "priority": "18",
+  "firstname": "Mudit",
+  "lastname": "Dwivedi",
+  "por": "Mech Lead (2021-2022)",
+  "image": "./team/Mudit_MechLead.png",
+  "linkedin": "https://www.linkedin.com/in/mudit-dwivedi-6186b5194/"
+},
+{
+  "year": "alpha",
+  "priority": "19",
+  "firstname": "Aman Kumar",
+  "lastname": "Chaurasia",
+  "por": "Mech Co-Lead (2021-2022)",
+  "image": "./team/Aman_Kumar_Chaurasia_Mechanical.png",
+  "linkedin": "https://www.linkedin.com/in/aman-kumar-chaurasia-1aa84a202"
+},
+{
+  "year": "alpha",
+  "priority": "20",
+  "firstname": "Ayush ",
+  "lastname": "Shekhar",
+  "por": "Science Lead (2021-2022)",
+  "image": "./team/Ayush_Shekhar_Science.png",
+  "linkedin": "https://www.linkedin.com/in/ayush-shekhar-4392ba1b6/"
+},
+{
+  "year": "alpha",
+  "priority": "21",
+  "firstname": "Siddhant",
+  "lastname": "Shrivastava",
+  "por": "Science Co-Lead (2021-2022)",
+  "image": "./team/Siddhant_Science.png",
+  "linkedin": "https://www.linkedin.com/in/siddhant-shrivastava-2b7b99203/"
+},
+{
+  "year": "alpha",
+  "priority": "22",
+  "firstname": "Adamya",
+  "lastname": "Garg",
+  "por": "Business Lead (2021-2022)",
+  "image": "./team/Adamya_Business.png",
+  "linkedin": "https://www.linkedin.com/in/adamya-garg-7725251b4/"
+},
+{
+  "year": "alpha",
+  "priority": "23",
+  "firstname": "Anushka",
+  "lastname": "Dutt",
+  "por": "Business Co-lead (2021-2022)",
+  "image": "./team/anushka_dutt.png",
+  "linkedin": "https://www.linkedin.com/in/anushka-dutt-834073214"
+},
+{
+  "year": "alpha",
+  "priority": "24",
+  "firstname": "Ujjwal",
+  "lastname": "Joshi",
+  "por": "Designing Head (2021-2022)",
+  "image": "./team/Ujjwal_Design.png",
+  "linkedin": "https://www.linkedin.com/in/ujjwal-joshi-348209214/"
+},
+{
+  "year": "alpha",
+  "priority": "25",
+  "firstname": "Shantanu",
+  "lastname": "Goyal",
+  "por": "Member (2021-2022)",
+  "image": "./team/shantanu_goyal.png",
+  "linkedin": "https://www.linkedin.com/in/shantanugoyal23"
+},
+{
+  "year": "beta",
+  "priority": "25",
+  "firstname": "Nishant",
+  "lastname": "Chahar",
+  "por": "President (2020-2021)",
+  "image": "./team/nishant_chahr.png"
+},
+{
+  "year": "beta",
+  "priority": "26",
+  "firstname": "Devansh",
+  "lastname": "Batra",
+  "por": "President (2020-2021)",
+  "image": "./team/devansh_batra.png"
+},
+{
+  "year": "beta",
+  "priority": "27",
+  "firstname": "Jitmanyu",
+  "lastname": "Anand",
+  "por": "Team Lead (2020-2021)",
+  "image": "./team/jitmanyu_anand.png"
+},
+{
+  "year": "beta",
+  "priority": "28",
+  "firstname": "Mohit",
+  "lastname": "Anant",
+  "por": "Science Co-Lead (2020-2021)",
+  "image": "./team/mohit_anant.png"
+},
+{
+  "year": "beta",
+  "priority": "29",
+  "firstname": "Amisha",
+  "lastname": "Bhaskar",
+  "por": "Mech Co-Lead (2020-2021)",
+  "image": "./team/amisha_bhaskar.png"
+},
+{
+  "year": "beta",
+  "priority": "30",
+  "firstname": "Pranav",
+  "lastname": "Arora",
+  "por": "Operation Team Lead (2020-2021)",
+  "image": "./team/pranav_arora.png"
+},
+{
+  "year": "beta",
+  "priority": "31",
+  "firstname": "Milind",
+  "lastname": "Jain",
+  "por": "Mech Co-Lead (2020-2021)",
+  "image": "./team/milind_jain.png"
+},
+{
+  "year": "beta",
+  "priority": "32",
+  "firstname": "Gaurav",
+  "lastname": "Verma",
+  "por": "Mech Team Lead (2020-2021)",
+  "image": "./team/gaurav_verma.png"
+},
+{
+  "year": "beta",
+  "priority": "33",
+  "firstname": "Anshita",
+  "lastname": "Goel",
+  "por": "Outreach Lead (2020-2021)",
+  "image": "./team/anshita_goel.png"
+},
+{
+  "year": "gamma",
+  "priority": "34",
+  "firstname": "Sanuj",
+  "lastname": "Kulshreshta",
+  "por": "President (2019-2020)",
+  "image": "./team/sanuj.png"
+},
+{
+  "year": "gamma",
+  "priority": "35",
+  "firstname": "Abhinav",
+  "lastname": "Gupta",
+  "por": "Electronics Lead (2019-2020)",
+  "image": "./team/abhinav_gupta.png"
+},
+{
+  "year": "gamma",
+  "priority": "36",
+  "firstname": "Manpreet",
+  "lastname": "Kuckreja",
+  "por": "Mech Team Lead (2019-2020)",
+  "image": "./team/manpreet_kuckreja.png"
+},
+{
+  "year": "delta",
+  "priority": "37",
+  "firstname": "Devansh",
+  "lastname": "Jain",
+  "por": "President (2018-2019)",
+  "image": "./team/devansh_jain.png"
+},
+{
+  "year": "delta",
+  "priority": "38",
+  "firstname": "Kunal",
+  "lastname": "Goel",
+  "por": "Electronics Lead (2018-2019)",
+  "image": "./team/kunal_goel.png"
+}];
+
+function yourFunction() {
   for (var i = 0; i < data.length; i++) {
     var outerdiv = document.createElement("div");
     var dataColour = document.createAttribute('data-color');
@@ -29,8 +363,8 @@ $.get("https://opensheet.elk.sh/1o4WOXewxr_uh70EzAjx9O7JwM3E7EUDrZrR0HrL3Src/1",
     div3.appendChild(por);
     $(outerdiv).appendTo('#portfolioList');
   }
+}
 
-});
 
 
 //For Contact Us
